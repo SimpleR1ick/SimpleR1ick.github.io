166451d1ae37bf7a3f060c4a121fed7745042caf
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2021-09-10 21:24:06 -0300

    Update Botao.js

--- Botao.js
+++ Botao.js
@@ -1,18 +1,15 @@
-document.addEventListener("DOMContentLoaded", inserirBotoes);
+document.addEventListener("DOMContentLoaded", inserirBotoesComInnerHTML);
 
-function inserirBotoes() {
+function inserirBotoesComInnerHTML(){
     div = document.createElement("div");
-    div2 = document.createElement("div");
-    div.innerHTML = '<button onclick="aumentarFonte()">+</button';
-    div2.innerHTML = '<button onclick="diminuirFonte()">-</button>';
+    div.innerHTML =`
+    <button onclick="mudartamanho('60')">+</button>
+    <button onclick="mudartamanho('25')">-</button>
+    `
     document.body.prepend(div);
-    document.body.prepend(div2);
 }
-function aumentarFonte() {
-    var titulo = document.getElementById('titulo');
-    titulo.style.fontSize="50px";
-  }
-  function diminuirFonte() {
-    var titulo = document.getElementById('titulo');
-    titulo.style.fontSize="25px";
-  }
+
+function mudartamanho(tamanho){
+    h2 = document.querySelector('h1');
+    h2.style.fontSize = tamanho+'px';
+}
