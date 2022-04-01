b9b07c187f333176b4adc9a67282bb42662c84b7
Henrique Dalmagro <84425302+SimpleR1ick@users.noreply.github.com>
2022-04-01 15:30:16 -0300

    Update

--- js/Botao.js
+++ js/Botao.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", inserirBotoesComInnerHTML);
 
-function inserirBotoesComInnerHTML(){
+function inserirBotoesComInnerHTML() {
     div = document.createElement("div");
-    div.innerHTML =`
+    div.innerHTML = `
     <button onclick="mudartamanho('60')">+</button>
-    <button onclick="mudartamanho('25')">-</button>
-    `
+    <button onclick="mudartamanho('25')">-</button>`
     document.body.prepend(div);
 }
 
-function mudartamanho(tamanho){
+function mudartamanho(tamanho) {
     h2 = document.querySelector('h1');
-    h2.style.fontSize = tamanho+'px';
+    h2.style.fontSize = tamanho + 'px';
 }
